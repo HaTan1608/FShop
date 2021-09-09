6c93cfc5aefcb3079e5e046fd6703e97299b35f3
HaTan1608 <63892386+HaTan1608@users.noreply.github.com>
2021-09-09 19:53:29 +0700

    update backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -29,10 +29,8 @@ app.get('/api/config/paypal', (req, res) => {
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-app.get('/', (req, res) => {
-  res.send('Server is ready');
-});
-
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/frontend/build/index.html')))
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
