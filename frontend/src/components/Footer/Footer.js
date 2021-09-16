3db0230dc9cf7f57952438fd8a75dc8d6879dd67
HaTan1608 <63892386+HaTan1608@users.noreply.github.com>
2021-09-16 15:34:43 +0700

    update

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -6,23 +6,24 @@ const Footer = () => {
     return (
         <footer className="footer" >
             <div className="footer__container">
+                <div className="container">
+                    <div className="footer__container__block">
+                        <div className="row ml-minus-15 mr-minus-15">
+                            <div className="col-6 p-15">
+                                <FooterIntro />
+                            </div>
+                            <div className="col-6 p-15">
+                                <FooterLinks />
+                            </div>
 
-                <div className="footer__container__block">
-                    <div className="row ml-minus-15 mr-minus-15">
-                        <div className="col-6 p-15">
-                            <FooterIntro />
                         </div>
-                        <div className="col-6 p-15">
-                            <FooterLinks />
-                        </div>
-
                     </div>
 
-                </div>
-                <div className="footer__bottom">
-                    <p>&copy; Copy right Fshop {new Date().getFullYear()}</p>
-                </div>
 
+                </div>
+            </div>
+            <div className="footer__bottom">
+                <p>&copy; Copy right Fshop {new Date().getFullYear()}</p>
             </div>
         </footer>
     )
